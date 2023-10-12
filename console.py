995ce073a683ccb57252cf12c2ddc2229d834f44
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-10-12 20:35:00 +0000

    updated basemodel and the console

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a program that contains the entry point of the command interpreter"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """provides a prompt for user input, allowing users to execute basic commands and exit the program."""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit on EOF (Ctrl+D)"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
