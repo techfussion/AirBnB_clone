e591a553898e2ed33ed425e56a45efb783bf9fa6
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-10-14 09:00:06 +0000

    serialization and deserialization of User

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,8 +38,31 @@ class FileStorage:
 
     def reload(self):
         """
-            Deserializes the JSON file to __objects
+            Reloads the stored objects from the JSON file
         """
-        if (os.path.isfile(__file_path)):
-            with open(__file_path, "r") as read_stream:
-                __objects = json.load(read_stream)
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v) if "__class__" in v else v for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
+    def attributes(self):
+        """
+        Returns the valid attributes and their types for each classname.
+
+        This method provides information about the expected attributes and their data types
+        for various classes used in the application.
+        """
+        attributes = {
+            "BaseModel": {"id": str, "created_at": datetime.datetime, "updated_at": datetime.datetime},
+            "User": {"email": str, "password": str, "first_name": str, "last_name": str},
+            "State": {"name": str},
+            "City": {"state_id": str, "name": str},
+            "Amenity": {"name": str},
+            "Place": {"city_id": str, "user_id": str, "name": str, "description": str,
+                      "number_rooms": int, "number_bathrooms": int, "max_guest": int,
+                      "price_by_night": int, "latitude": float, "longitude": float, "amenity_ids": list},
+            "Review": {"place_id": str, "user_id": str, "text": str}
+        }
+        return attributes
+
