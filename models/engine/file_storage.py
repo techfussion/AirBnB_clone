d58b90da13b253ae268adf31ae271ae9fe6d9f05
Emmanuel Kisanda <103537166+Kisanda25@users.noreply.github.com>
2023-10-15 18:48:51 +0200

    manage correctly serialization and deserialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,9 +38,50 @@ class FileStorage:
             json.dump(self._FileStorage__objects, write_stream)
 
     def reload(self):
-        """
-            Deserializes the JSON file to __objects
-        """
-        if (os.path.isfile(self._FileStorage__file_path)):
-            with open(self._FileStorage__file_path, "r") as read_stream:
-                self.__objects = json.load(read_stream)
+        """Reload stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """This returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
