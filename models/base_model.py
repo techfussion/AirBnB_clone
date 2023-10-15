3a6e68a6b2b379e33cc2327dc50958f63034c45c
Emmanuel Kisanda <103537166+Kisanda25@users.noreply.github.com>
2023-10-15 23:04:27 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -14,17 +14,18 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        strptime_format = "%Y-%m-%dT%H:%M:%S.5f"
-                        setattr(self, key,
-                                datetime.strptime(value, strptime_format))
-                    else:
-                        setattr(self, key, value)
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             storage.new(self)
